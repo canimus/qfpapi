6a24cbd1cd036451942025fd623d89edeb9a976e
Herminio Vazquez <canimus@gmail.com>
2017-10-25 16:35:34 +0200

    Token invalid debugging

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 # Stop and remove all running containers
 docker ps -a | xargs docker rm -f
 
@@ -10,7 +8,7 @@ docker images --filter "dangling=true" -q | xargs docker rmi
 docker images --filter=reference='qfpapi:*' -q | xargs docker rmi
 
 # Build new image
-docker build -f qfpapi:1.0.0 .
+docker build -t qfpapi:1.0.0 .
 
 # Run image
 docker run -d --name qfpapi -p 5000:5000 qfpapi:1.0.0
